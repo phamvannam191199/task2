1885bba37c3d2ea9a321b78a27a8e9bec3d55735
phamvannam191199 <82355164+phamvannam191199@users.noreply.github.com>
2021-04-22 18:53:48 +0700

    Add files via upload

---
+++ bai2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main (){
+int i;
+float tien;
+printf("Nhap so tien can chuyen \n");
+scanf("%d",&i);
+tien=(float) i*16.689;
+printf(" Tien USD duoc doi ra la %f ",tien);
+getch();
+}
+ 
+
+
